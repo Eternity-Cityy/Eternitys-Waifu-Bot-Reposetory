f86d6abd361cad55fffba1c512764f899184d0e1
Eternity-Scripting <94948595+Eternity-Scripting@users.noreply.github.com>
2022-01-19 22:20:56 -0600

    create

---
+++ Index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+
+const TOKEN = ("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "Hi Waifu") {
+        message.reply("Hello <3")
+    }
+})
+
+client.login(pricess.env.TOKEN)
